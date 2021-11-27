5781772c3afc258fea0c9cf5dd4095edc6494a93
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-11-27 12:20:03 -0800

    completed

--- node-mongo/server.js
+++ node-mongo/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.json())
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
 require('./app/models/inventory.model.js');
 
 //configuring the database
